4d1acdef9d44cb95c9cd637b276968b2c3092fa7
ievajoma <162178511+ievajoma@users.noreply.github.com>
2025-01-07 19:40:51 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,2 @@
+<h1>Welcome to Laravel!</h1>
+<p>Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.</p>
